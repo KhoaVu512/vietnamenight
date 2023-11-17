48c462dbb67a72353a7efb6d78573b3aa8423921
VincenzoKhoa <khoa.vm.512@gmail.com>
2023-11-17 21:16:42 +0700

    fix 17

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,59 +10,57 @@ import TopLocation from '@/app/home/top-location'
 import Album from '@/app/home/album'
 
 function PageHome() {
-
-    return (
-      <div className="flex min-h-screen flex-col items-center">
-        <LayoutPage />
-        <div className="w-full h-[75%]">
-          <img
-            src="https://vietnamnightlife.com/uploads/images/2020/07/1594608182-single_banner26-vietnamnightlifeslider.jpg.webp"
-            alt=""
-            className="w-full "
-          />
-        </div>
-        <div className=" bg-[#111111] w-full">
-          <div className="flex justify-center">
-            <IntroHome />
-          </div>
-        </div>
-        <div>
-          <EventHome />
-          <TopLocation />
-        </div>
-        <div >
-          <img
-            src="https://vietnamnightlife.com/uploads/images/2022/10/1665120200-single_banner45-z376243728949754c7499d23a8643505f533d763f4ab35.png.webp"
-            alt=""
-            className="w-full h-[40%] mt-20"
-          />
-        </div>
-        <div>
-          <ComboNight />
-        </div>
-  
-        <div>
-          <img
-            src="https://vietnamnightlife.com/uploads/images/2022/10/1665120200-single_banner45-z376243728949754c7499d23a8643505f533d763f4ab35.png.webp"
-            alt=""
-            className="w-full h-[40%] mt-20"
-          />
-        </div>
-  
-        <div>
-          <PrivateParty />
-        </div>
-        <div>
-          <NightlifeStory />
-        </div>
-        <div>
-          <Album />
-        </div>
-  
-        <div className="flex w-[1214px] h-screen "></div>
+  return (
+    <div className="flex min-h-screen flex-col items-center">
+      <LayoutPage />
+      <div className="w-full h-[75%]">
+        <img
+          src="https://vietnamnightlife.com/uploads/images/2020/07/1594608182-single_banner26-vietnamnightlifeslider.jpg.webp"
+          alt=""
+          className="w-full "
+        />
       </div>
-    )
-  
+      <div className=" bg-[#111111] w-full">
+        <div className="flex justify-center">
+          <IntroHome />
+        </div>
+      </div>
+      <div>
+        <EventHome />
+        <TopLocation />
+      </div>
+      <div>
+        <img
+          src="https://vietnamnightlife.com/uploads/images/2022/10/1665120200-single_banner45-z376243728949754c7499d23a8643505f533d763f4ab35.png.webp"
+          alt=""
+          className="w-full h-[40%] mt-20"
+        />
+      </div>
+      <div>
+        <ComboNight />
+      </div>
+
+      <div>yar
+        <img
+          src="https://vietnamnightlife.com/uploads/images/2022/10/1665120200-single_banner45-z376243728949754c7499d23a8643505f533d763f4ab35.png.webp"
+          alt=""
+          className="w-full h-[40%] mt-20"
+        />
+      </div>
+
+      <div>
+        <PrivateParty />
+      </div>
+      <div>
+        <NightlifeStory />
+      </div>
+      <div>
+        <Album />
+      </div>
+
+      <div className="flex w-[1214px] h-screen "></div>
+    </div>
+  )
 }
 
 export default PageHome
